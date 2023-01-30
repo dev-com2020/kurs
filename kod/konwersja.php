d3b2266b060c46bdeb1d76241db6f28867768f47
devcom2020 <kwp@tlen.pl>
2023-01-30 12:58:47 +0100

    poniedzialek 12:58

--- kod/konwersja.php
+++ kod/konwersja.php
@@ -4,9 +4,12 @@
 // $napis3 = $napis1 + $napis2;
 // echo $napis3;
 $liczba1 = 1.5;
+$napis = "1Tomek";
+$nowy_napis = (int) $napis;
+echo $nowy_napis;
 echo $liczba1;
 // $liczba2 = (int) $liczba1;
 $liczba2 = intval($liczba1);
 echo "<br>";
-echo $liczba2;
+echo 'Zmienna $liczba2 jest typu: ' .gettype($liczba2);
 ?>
