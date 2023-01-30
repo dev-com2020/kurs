de1619879a012ef3f5c9e79d5d72898b09e75fc9
devcom2020 <kwp@tlen.pl>
2023-01-30 14:01:12 +0100

    poniedzialek 14:01

--- kod/warunki.php
+++ kod/warunki.php
@@ -1,15 +1,34 @@
 <?php
-$liczba = 0;
+$liczba = 20;
+$napis = 5;
 
 if ($liczba < 0){
     echo "zmienna ma wartość $liczba i jest mniejsza od zera.";
 }
 else if ($liczba > 0){
     echo "zmienna ma wartość $liczba i jest większa od zera.";
+    if ($napis === ""){
+        echo "<br>Zmienna $napis jest pusta i jest w typie string";
+    }
+    else{
+        echo "<br>Zmienna nie jest pusta";
+    }
 }
 else{
     echo "zmienna ma wartość $liczba.";
 }
 
-echo "<br>Tutaj inny kod...";
+echo "<br>Tutaj inny kod...\n";
+
+if(($liczba > 5) && ($liczba < 20)){
+    echo "\ninstrukcja się wykonuje...";
+}
+
+if(($liczba == 5) || ($liczba == 20)){
+    echo "\ninstrukcja się wykonuje drugi raz...";
+}
+if(($liczba <= 100 && $liczba >= 10) && ($licznik != 25 || $licznik == 15)){
+    // instrukcje
+}
+
 ?>
