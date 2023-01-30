2e637e0adf274e982a87bcbe01c00578c8829f83
devcom2020 <kwp@tlen.pl>
2023-01-30 11:53:59 +0100

    czysty index

--- index.php
+++ index.php
@@ -10,20 +10,8 @@
     <div>
         <h1>The Heading</h1>
         <p><b>Strona testowa</b><br>
-        <?php include("kod/skrypt.php");?>
-        <br>
-        <?php include("kod/napisy.php");?>
-        <!-- <?php require("skrypt2.php");?> -->
-            <!-- <script language="php">
-                echo "<h2 style='text-align:center;'>Test PHP nr 2:)</h2>";
-            </script>
-            <?
-                echo "<h2 style='text-align:center;'>Test PHP nr 2:)</h2>";
-            ?>
-            <%
-                echo "<h2 style='text-align:center;'>Test PHP nr 2:)</h2>";
-            %> -->
-            <br>To jest dalsza część strony....
+
+
         </p>        
     </div>
 </body>
