a5d9212f12e7a621625613ea33adb60a3dae5a5a
devcom2020 <kwp@tlen.pl>
2023-01-30 16:01:36 +0100

    poniedzialek 16:01

--- kod/funkcja.php
+++ kod/funkcja.php
@@ -3,10 +3,30 @@ function funkcja(){
     echo "Instrukcje...<br>";
 }
 
-function razyDwa($x){
-    $wynik = $x * 2;
-    echo "pomnożone: $x * 2 = $wynik<br>";
+// function razyDwa($x){
+//     $wynik = $x * 2;
+//     echo "pomnożone: $x * 2 = $wynik<br>";
+// }
+function razyDwa($x):int{
+   return $x * 2.5;
 }
+
+
+function jakaWartosc($liczba):string{
+    if($liczba < 0){
+        return "ujemna";
+    } else if ($liczba > 0){
+        return "dodatnia";
+    } else {
+        return "zero";
+    }
+    }
+
+// echo jakaWartosc(2);
+// echo "<br>";
+// echo jakaWartosc(-2);
+// echo "<br>";
+// echo jakaWartosc(0);
 
 function dodaj($x,$y){
     $wynik = $x + $y;
@@ -14,8 +34,26 @@ function dodaj($x,$y){
 }
 
 
-for ($i = 0; $i < 11; $i++):
-    razyDwa($i);
-    dodaj($i,$i);
-endfor;
+// $wynik = razyDwa(5);
+// echo "$wynik";
+
+// for ($i = 0; $i < 11; $i++):
+//     razyDwa($i);
+//     dodaj($i,$i);
+// endfor;
+
+$a = 1;
+function f(){
+    // global $a;
+    global $liczba;
+    $liczba = 100;
+    $a = 10000;
+    echo "Wartość a = $a <br>";
+    echo "Wartość liczba = $liczba <br>";
+}
+
+f();
+echo "Wartość a = $a <br>";
+echo "Wartość liczba = $liczba <br>";
+
 ?>
